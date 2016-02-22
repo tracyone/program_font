5b059b516d2124c4721670777656a11ff3fd3e80
tracyone <tracyone@live.cn>
2016-02-22 22:48:51 +0800

    添加echo

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # author:tracyone,tracyone@live.cn
 
+echo "Install start..."
 sudo chmod -R a+x *
 sudo mkdir -p /usr/share/fonts/MyFonts
 mkdir ~/.fonts/
 cp * ~/.fonts/
 sudo cp * /usr/share/fonts/MyFonts
 sudo fc-cache -f -v
+echo "Install finish..."
