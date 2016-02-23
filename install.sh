8f1653cc3bbe4a59f677315bd73bf5650758a114
tracyone <tracyone@live.cn>
2016-02-23 10:54:16 +0800

    使用正则表达式来拷贝安装字体

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 echo "Install start..."
 sudo chmod -R a+x *
 sudo mkdir -p /usr/share/fonts/MyFonts
-mkdir ~/.fonts/
-cp * ~/.fonts/
-sudo cp * /usr/share/fonts/MyFonts
+mkdir -p ~/.fonts/
+find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec cp {} ~/.fonts/  \;
+find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec sudo cp {} /usr/share/fonts/MyFonts  \;
 sudo fc-cache -f -v
 echo "Install finish..."
