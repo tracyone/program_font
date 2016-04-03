908d423f0b4fda2d4a4397f639b26364a69fcbe2
tracyone <tracyone@live.cn>
2016-04-03 12:15:02 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # author:tracyone,tracyone@live.cn
 
+if [[ `uname` == 'Darwin' ]]; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts/"
+else
+  # Linux
+  font_dir="${HOME}/.fonts/"
+  mkdir -p $font_dir
+  sudo mkdir -p /usr/share/fonts/MyFonts
+  find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec sudo cp {} /usr/share/fonts/MyFonts  \;
+fi
+
 echo "Install start..."
-sudo chmod -R a+x *
-sudo mkdir -p /usr/share/fonts/MyFonts
-mkdir -p ~/.fonts/
-find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec cp {} ~/.fonts/  \;
-find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec sudo cp {} /usr/share/fonts/MyFonts  \;
+find . -regextype posix-egrep -iregex '.*\.tt[cf]$' -exec cp {} ${font_dir}  \;
 sudo fc-cache -f -v
 echo "Install finish..."
